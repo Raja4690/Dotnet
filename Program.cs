38a5ad6370ff9c1e7e6827ad1a59b48e84aaf743
Raja4690 <112933411+Raja4690@users.noreply.github.com>
2022-10-03 10:32:23 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+ class dk
+{
+
+    static void Main(string[] args)
+    {
+
+         List<int> my = new List<int>() { 78, 460, 780, 890, 303, 170 };
+        {
+           // string[] my = new string[] { "Aman", "Raman", "Shyam", "Aditya" };
+
+            var raja = from w in my
+                       orderby w
+                       select w;
+
+
+            Console.WriteLine("Sorted in Ascending order by length:");
+            foreach (int value in raja)
+            {
+                Console.Write(value + " ");
+            }
+        }
+    }
+}
